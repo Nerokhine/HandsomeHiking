ecf152064df0b61d9cc482f68a70c9b0ef4ab8c2
Nerokhine <lipton1@live.ca>
2017-12-09 11:06:00 -0500

    cleanup

--- SexyHiking/Assets/Scripts/MouseController.cs
+++ SexyHiking/Assets/Scripts/MouseController.cs
@@ -13,13 +13,6 @@ public class MouseController : MonoBehaviour {
 	bool once = true;
 	bool once2 = true;
 	bool clockwise = true;
-	// Update is called once per frame
-
-	void LateUpdate(){
-		if (yes) {
-			//GameObject.Find ("LargeWheel").GetComponent<HingeJoint2D> ().connectedBody = hand.GetComponent <Rigidbody2D>();
-		}
-	}
 
 	void Update(){
 		GameObject.Find ("Main Camera").GetComponent<Camera> ().transform.position = 
@@ -38,7 +31,6 @@ public class MouseController : MonoBehaviour {
 
 		float angle2 = hand.transform.localEulerAngles.z;
 		angle2 = ((angle2 + 270f) % 360f);
-		//Debug.Log (angle2);
 
 		Vector3 screenPoint = Input.mousePosition;
 		screenPoint.z = 5.0f; //distance of the plane from the camera
@@ -51,37 +43,8 @@ public class MouseController : MonoBehaviour {
 
 		angle = ((Mathf.Rad2Deg * angle + 90f) % 360f);
 
-		/*if (angle < 180) {
-			if (once) {
-				foreach (PolygonCollider2D collider in hand.GetComponents<PolygonCollider2D>()) {
-					if (collider.isActiveAndEnabled) {
-						collider.enabled = false;
-					} else {
-						collider.enabled = true;
-					}
-				}
-				hand.GetComponent<SpriteRenderer> ().sprite = Resources.Load <Sprite>("YellowHand2");
-				once = false;
-			}
-			once2 = true;
-		} else {
-			if (once2) {
-				foreach (PolygonCollider2D collider in hand.GetComponents<PolygonCollider2D>()) {
-					if (collider.isActiveAndEnabled) {
-						collider.enabled = false;
-					} else {
-						collider.enabled = true;
-					}
-				}
-				hand.GetComponent<SpriteRenderer> ().sprite = Resources.Load <Sprite>("YellowHand");
-				once2 = false;
-			}
-			once = true;
-		}*/
-		//angle += Mathf.PI/2f;
-		//angle = 90 - angle;
 		float distance = Mathf.Sqrt(Mathf.Pow(dudePos.y - mousePos.y, 2) +  Mathf.Pow(dudePos.x - mousePos.x, 2));
-		//Debug.Log ("Distance: " + distance.ToString());
+		Debug.Log ("Distance: " + distance.ToString());
 		if (distance < 2f) {
 			blob.GetComponent<HingeJoint2D> ().connectedAnchor = connectedAnchor - (connectedAnchor - connectedAnchor * (distance) / 2f);
 			if (distance < .9f && distance > .7f) {
@@ -93,35 +56,16 @@ public class MouseController : MonoBehaviour {
 				hand.transform.localScale = new Vector3 (.05f, .05f, 1f);
 			} else {
 				hand.transform.localScale = new Vector3 (0.625f, 0.625f, 1);
-				//blob.GetComponent<HingeJoint2D> ().connectedAnchor = connectedAnchor - (connectedAnchor - connectedAnchor * (distance - .7f) / 1.3f);
 			}
 			if (blob.GetComponent<HingeJoint2D> ().connectedAnchor.x < 0f && !yes) {
-				//Destroy (GameObject.Find ("LargeWheel").GetComponent<HingeJoint2D> ());
-				//hand.transform.eulerAngles = new Vector3(hand.transform.eulerAngles.x, hand.transform.eulerAngles.y, hand.transform.eulerAngles.z - 180);
 				yes = true;
-				/*HingeJoint2D hinge = new HingeJoint2D ();
-				hinge.connectedAnchor = GameObject.Find ("LargeWheel").GetComponent<HingeJoint2D> ().connectedAnchor;
-				hinge.connectedBody = GameObject.Find ("LargeWheel").GetComponent<HingeJoint2D> ().connectedBody;
-
-				Destroy(GameObject.Find ("LargeWheel").GetComponent<HingeJoint2D>());
-				HingeJoint2D myHinge = gameObject.AddComponent(typeof(HingeJoint2D)) as HingeJoint2D;
-				myHinge.connectedBody = hinge.connectedBody;
-				myHinge.connectedAnchor = hinge.connectedAnchor;*/
 			}
-			//GameObject.Find ("LargeWheel").GetComponent<HingeJoint2D> ().anchor = connectedAnchor * (1f - distance);
 		} else {
 			blob.GetComponent<HingeJoint2D> ().connectedAnchor = connectedAnchor;
 			yes = false;
 		}
-
-		/*if (mousePos.x < dudePos.x && mousePos.y < dudePos.y) {
-			angle += 180;
-		} else if (mousePos.x < dudePos.x && mousePos.y > dudePos.y) {
-			angle += 270;
-		} else if (mousePos.x > dudePos.x && mousePos.y < dudePos.y) {
-			angle += 90;
-		}*/
-		//Debug.Log ("Angle: " + angle);
+			
+		Debug.Log ("Angle: " + angle);
 
 		Collider2D [] colliders = GameObject.FindObjectsOfType<Collider2D>();
 		last = Input.mousePosition;
@@ -139,12 +83,6 @@ public class MouseController : MonoBehaviour {
 				break;
 			}
 		}
-			
-
-		/*if (Input.GetMouseButton (0)) {
-			GameObject.Find ("Dingle").GetComponent<Rigidbody2D> ().velocity = new Vector2 (0, 0);
-			GameObject.Find ("LargeWheel").GetComponent<HingeJoint2D> ().useMotor = false;*/
-			//Debug.Log ("hi");
 		
 		blob.GetComponent<HingeJoint2D> ().useMotor = true;
 		JointMotor2D motor = new JointMotor2D ();
@@ -249,7 +187,6 @@ public class MouseController : MonoBehaviour {
 			}
 		}
 
-		Debug.Log (motor.motorSpeed);
 		blob.GetComponent<HingeJoint2D> ().motor = motor;
 
 	}
