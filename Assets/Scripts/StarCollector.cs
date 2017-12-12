0f0aa53f27d0b09cbbab658b9c70611ce04dc557
Nerokhine <lipton1@live.ca>
2017-12-11 23:42:01 -0500

    added collectable star

---
+++ Assets/Scripts/StarCollector.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StarCollector : MonoBehaviour {
+	public static bool collected = false;
+	PolygonCollider2D blobCollider;
+	// Use this for initialization
+	void Start () {
+		blobCollider = GameObject.Find ("Blob").GetComponent<PolygonCollider2D> ();
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		if (GetComponent<PolygonCollider2D> ().IsTouching (blobCollider)) {
+			collected = true;
+			Destroy (gameObject);
+		}
+	}
+}
