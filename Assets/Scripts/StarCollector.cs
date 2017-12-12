e9380efb15ff0592d1c9d359a45a7d8699ae4cd5
Nerokhine <lipton1@live.ca>
2017-12-11 23:47:14 -0500

    made star collider trigger from hand too

--- Assets/Scripts/StarCollector.cs
+++ Assets/Scripts/StarCollector.cs
@@ -5,14 +5,16 @@ using UnityEngine;
 public class StarCollector : MonoBehaviour {
 	public static bool collected = false;
 	PolygonCollider2D blobCollider;
+	PolygonCollider2D handCollider;
 	// Use this for initialization
 	void Start () {
 		blobCollider = GameObject.Find ("Blob").GetComponent<PolygonCollider2D> ();
+		handCollider = GameObject.Find ("Hand").GetComponent<PolygonCollider2D> ();
 	}
 	
 	// Update is called once per frame
 	void FixedUpdate () {
-		if (GetComponent<PolygonCollider2D> ().IsTouching (blobCollider)) {
+		if (GetComponent<PolygonCollider2D> ().IsTouching (blobCollider) || GetComponent<PolygonCollider2D> ().IsTouching (handCollider)) {
 			collected = true;
 			Destroy (gameObject);
 		}
